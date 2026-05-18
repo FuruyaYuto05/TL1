ffc7809d34b968ba905c9822ba40917034615bce
FuruyaYuto <k024g0025@g.neec.ac.jp>
2026-05-18 12:23:24 +0900

    Update level_editor.py
    01_04のP12まで

--- level_editor.py
+++ level_editor.py
@@ -15,12 +15,23 @@ bl_info = {
 
 #アドオン有効化時コールバック
 def register():
+    #メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(draw_menu_manual)
     print("レベルエディタが有効化されました")
     
 #アドオン無効化時コールバック
 def unregister():
+    #メニューから項目を削除
+    bpy
     print("レベルエディタが無効化されました")
     
     
+def draw_menu_manual(self, context):
+    #self : 呼び出し元のクラスインスタンス C++で言うthisポインタ
+    #context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+    #トップバーの「エディターメニュー」に項目（オペレーター）を追加
+    self.layout.operator("wm.url_open", text="Manual",icon='HELP')
+    
+
 if __name__ == "__main__":
    register()
