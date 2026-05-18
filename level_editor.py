0ebdaa0628fa8e762a37bbf228e30a326eb4ee82
FuruyaYuto <k024g0025@g.neec.ac.jp>
2026-05-18 13:33:03 +0900

    Update level_editor.py
    01_04完成

--- level_editor.py
+++ level_editor.py
@@ -13,16 +13,26 @@ bl_info = {
     "category": "Object",
 }
 
+
 #アドオン有効化時コールバック
 def register():
+    #クラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)  
+
     #メニューに項目を追加
-    bpy.types.TOPBAR_MT_editor_menus.append(draw_menu_manual)
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
     
 #アドオン無効化時コールバック
 def unregister():
     #メニューから項目を削除
-    bpy
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    
+    #クラスを登録解除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    
     print("レベルエディタが無効化されました")
     
     
@@ -32,6 +42,25 @@ def draw_menu_manual(self, context):
     #トップバーの「エディターメニュー」に項目（オペレーター）を追加
     self.layout.operator("wm.url_open", text="Manual",icon='HELP')
     
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "My Menu"
+    bl_description = "拡張メニュー by" + bl_info["author"]
+
+    #サブメニュー描画
+    def draw(self, context):
+       #トップバーの「エディターメニュー」に項目（オペレーター）を追加
+       self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+    #既存のメニューにサブメニュー追加
+    def submenu(self, context):
+       #ID名を指定して、サブメニューを追加
+       self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+#Blemderに登録するクラスリスト
+classes = (
+    TOPBAR_MT_my_menu,
+)
 
 if __name__ == "__main__":
    register()
